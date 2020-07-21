20db31567b9a8050ae9ead6381444e971394ce4e
devdave0 <dbchung24@gmail.com>
2020-07-21 11:36:56 -0400

    finished add

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,4 +1,15 @@
 class PokemonsController < ApplicationController
 
-    
+    def index 
+        pokemons = Pokemon.all 
+
+        render json: pokemons 
+    end 
+
+    def create 
+        name = Faker::Name.first_name 
+        species = Faker::Games::Pokemon.name
+        pokemon = Pokemon.create(nickname: name, species: species, trainer_id: params[:trainer_id])
+        render json: pokemon
+    end 
 end
