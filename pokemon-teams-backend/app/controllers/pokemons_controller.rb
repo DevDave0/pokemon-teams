3058695e8cee1daa7c8797187417308c970c1ab1
devdave0 <dbchung24@gmail.com>
2020-07-21 13:10:56 -0400

    done

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -12,4 +12,11 @@ class PokemonsController < ApplicationController
         pokemon = Pokemon.create(nickname: name, species: species, trainer_id: params[:trainer_id])
         render json: pokemon
     end 
+
+    def destroy 
+        pokemon = Pokemon.find(params[:id])
+        pokemon.destroy 
+        render json: pokemon
+    end 
+
 end
